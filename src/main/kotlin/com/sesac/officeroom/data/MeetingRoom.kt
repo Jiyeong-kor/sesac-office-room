9000ed2f08553dfdcd99c132e0b3a83c95192fc3
kanakhan <cjh1950@naver.com>
2024-10-29 23:08:46 +0900

    Update MeetingRoom.kt
    enum class 주석 수정

--- src/main/kotlin/com/sesac/officeroom/data/MeetingRoom.kt
+++ src/main/kotlin/com/sesac/officeroom/data/MeetingRoom.kt
@@ -1,5 +1,7 @@
 package com.sesac.officeroom.data
-
+/*
+회의실 정보를 담고있는 enum class
+ */
 enum class MeetingRoom ( //회의실 정보를 담고있는 enum class
     val minCapacity: Int, //최소 인원
     val maxCapacity: Int, //최대 수용 가능 인원
@@ -8,9 +10,9 @@ enum class MeetingRoom ( //회의실 정보를 담고있는 enum class
     val hasPhotoBooth: Boolean //촬영부스 유무
 ){
     JJANG_GOOD_ROOM(2,4,5000,true,true),
-    //짱좋은 회의실: 최소인원 4명, 최대인원 12명, 기본요금 5000원, 창문있음, 포토부스 있음
+    //짱좋은 회의실: 최소인원 2명, 최대인원 4명, 기본요금 5000원, 창문있음, 포토부스 있음
     VERY_GOOD_ROOM(3,6,3000,true,false),
-    //좋은 회의실: 최소인원 2명, 최대인원 6명, 기본요금 3000원, 창문있음, 포토부스 없음
+    //좋은 회의실: 최소인원 3명, 최대인원 6명, 기본요금 3000원, 창문있음, 포토부스 없음
     LESS_GOOD_ROOM(4,12,2000,false,false)
     //덜좋은 회의실: 최소인원 4명, 최대인원 12명, 기본요금 2000원, 창문이랑 포토부스 없음
 }
